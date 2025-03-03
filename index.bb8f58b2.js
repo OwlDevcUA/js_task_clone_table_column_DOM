590142fb51362c424703436f2bb7afdbc86193d0
Owk <kkoctuk2005@gmail.com>
2025-03-03 20:41:59 +0200

    make build

---
+++ index.bb8f58b2.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").rows).forEach(function(e){var r=e.cells[1],l=e.cells.length-1;e.insertCell(l).innerHTML=r.innerHTML});
+//# sourceMappingURL=index.bb8f58b2.js.map
